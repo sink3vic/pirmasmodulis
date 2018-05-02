b2f514808b7a2e32b08fb3116d1cd6f48d710bb1
Giedrius <37585632+sink3vic@users.noreply.github.com>
2018-05-02 20:10:52 +0300

    21uzduotis su prisijungimo vardu

---
+++ php/21uzduotis.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        div{
+            margin-bottom:10px;
+        }
+        button{
+            margin-bottom:10px;
+        }
+    </style>
+</head>
+<body>
+    <h>Prisijungimas</h>
+    <div><input>Login</div>
+    <div><input>Password</div>
+    <button>Prisijungti</button>
+    
+    <?php 
+    
+    
+    echo "<div>&#169; ". date(Y) ."</div>";
+      
+        ?>
+</body>
+</html>
