29ce944a19de447b581bf63289dd4e113c70d444
Giedrius <37585632+sink3vic@users.noreply.github.com>
2018-05-03 14:22:35 +0300

    tinklapio biski ir php pavyzdziai klasej

---
+++ php/pratimas1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>login</h1>
+    <form action="login.php" method="post">
+        <input name="user" placeholder="user">
+        <input type="password" name="pass" placeholder="password">
+        <button>Login</button>
+    </form>
+</body>
+</html>
---
+++ php/pratimas1/login.php
@@ -0,0 +1,10 @@
+<?php
+    if($_POST['user'] == 'Jonas' && $_POST['pass'] == '123'){
+      //  echo "YES";
+    include "info.php";   
+    }
+    else {
+       // echo "no";
+       include "index.php";
+    }
+?>
